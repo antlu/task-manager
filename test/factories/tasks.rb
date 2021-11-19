d81e40e1cecebe1ee157793a72fbcc4e0da2a6bc
Anton <web-gambit@mail.ru>
2021-11-20 03:54:25 +0500

    Configure relations and validation

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     name { generate(:string) }
     description { generate(:string) }
-    author_id { 1 }
-    assignee_id { 1 }
+    author { association :manager }
+    assignee { association :developer }
     state { generate(:string) }
     expired_at { generate(:date) }
   end
