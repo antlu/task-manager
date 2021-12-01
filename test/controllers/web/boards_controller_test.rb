015da35fc922a6e0ce84d3a70d56d1c8313bdbc7
Anton <web-gambit@mail.ru>
2021-12-01 11:16:20 +0500

    Add authorization control for the board

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionController::TestCase
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
+  test 'should redirect on get show' do
+    sign_out
+    get :show
+    assert_response :redirect
+  end
+
   test 'should get show' do
     get :show
     assert_response :success
