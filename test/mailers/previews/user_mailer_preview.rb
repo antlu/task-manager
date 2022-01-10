b38f702216399a862646d72c0d6fdf6be5c856c5
Anton <web-gambit@mail.ru>
2022-01-10 19:48:45 +0500

    Add previews

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,20 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    user = User.first
+    task = Task.first
+    UserMailer.with({ user: user, task: task }).task_created
+  end
 
+  def task_edited
+    user = User.first
+    task = Task.first
+    UserMailer.with({ user: user, task: task }).task_edited
+  end
+
+  def task_deleted
+    user = User.first
+    task = Task.first
+    UserMailer.with({ user: user, task: task }).task_deleted
+  end
 end
