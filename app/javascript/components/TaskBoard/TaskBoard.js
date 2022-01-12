abc4aa13130f59e1d75d2c06b924809a2c0a064e
Anton <web-gambit@mail.ru>
2022-01-12 12:30:43 +0500

    Rearrange code

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -6,10 +6,8 @@ import '@asseinfo/react-kanban/dist/styles.css';
 
 import ColumnHeader from 'components/ColumnHeader';
 import Task from 'components/Task';
-import TaskForm from 'forms/TaskForm.js';
 import AddPopup from 'components/AddPopup';
 import EditPopup from 'components/EditPopup';
-import TasksRepository from 'repositories/TasksRepository.js';
 import TaskPresenter from 'presenters/TaskPresenter.js';
 import useTasksActions from 'hooks/useTasksActions.js';
 import useTasks from 'hooks/useTasks.js';
@@ -28,19 +26,9 @@ function TaskBoard() {
   const [mode, setMode] = useState(MODES.NONE);
   const [openTaskId, setOpenTaskId] = useState(null);
 
-  const { loadColumn, loadColumnMore } = useTasksActions();
+  const { loadColumnMore, changeTaskState, createTask, updateTask, deleteTask, loadTask } = useTasksActions();
 
-  const handleCardDragEnd = (task, source, destination) => {
-    const transition = TaskPresenter.transitions(task).find(({ to }) => destination.toColumnId === to);
-    if (!transition) return null;
-
-    return TasksRepository.update(TaskPresenter.id(task), { ...task, state: transition.to })
-      .then(() => loadColumn(source.fromColumnId))
-      .then(() => loadColumn(destination.toColumnId))
-      .catch((error) => {
-        alert(`Move failed! ${error.message}`);
-      });
-  };
+  const handleCardDragEnd = (task, source, destination) => changeTaskState(task, source, destination);
 
   const handleOpenAddPopup = () => setMode(MODES.ADD);
 
@@ -56,29 +44,13 @@ function TaskBoard() {
     setOpenTaskId(null);
   };
 
-  const handleCardCreate = (params) => {
-    const attributes = TaskForm.attributesToSubmit(params);
-    return TasksRepository.create(attributes).then(({ data: { task } }) => {
-      loadColumn(TaskPresenter.state(task));
-      handleCloseAddPopup();
-    });
-  };
+  const handleCardCreate = (params) => createTask(params).then(() => handleCloseAddPopup());
 
-  const handleCardLoad = (id) => TasksRepository.show(id).then(({ data: { task } }) => task);
+  const handleCardLoad = (id) => loadTask(id);
 
-  const handleCardUpdate = (task) => {
-    const attributes = TaskForm.attributesToSubmit(task);
-    return TasksRepository.update(TaskPresenter.id(task), attributes).then(() => {
-      loadColumn(TaskPresenter.state(task));
-      handleCloseEditPopup();
-    });
-  };
+  const handleCardUpdate = (params) => updateTask(params).then(() => handleCloseEditPopup());
 
-  const handleCardDestroy = (task) =>
-    TasksRepository.destroy(TaskPresenter.id(task)).then(() => {
-      loadColumn(TaskPresenter.state(task));
-      handleCloseEditPopup();
-    });
+  const handleCardDestroy = (task) => deleteTask(task).then(() => handleCloseEditPopup());
 
   useEffect(() => loadBoard(), []);
 
--- app/javascript/hooks/useTasksActions.js
+++ app/javascript/hooks/useTasksActions.js
@@ -2,6 +2,8 @@ import { useDispatch } from 'react-redux';
 
 import { loadColumnSuccess, loadColumnMoreSuccess } from 'slices/tasksSlice.js';
 import TasksRepository from 'repositories/TasksRepository.js';
+import TaskPresenter from 'presenters/TaskPresenter.js';
+import TaskForm from 'forms/TaskForm.js';
 
 export default () => {
   const dispatch = useDispatch();
@@ -21,8 +23,40 @@ export default () => {
 
   const loadColumnMore = makeColumnLoader(loadColumnMoreSuccess);
 
+  const changeTaskState = (task, source, destination) => {
+    const transition = TaskPresenter.transitions(task).find(({ to }) => destination.toColumnId === to);
+    if (!transition) return null;
+
+    return TasksRepository.update(TaskPresenter.id(task), { ...task, state: transition.to })
+      .then(() => loadColumn(source.fromColumnId))
+      .then(() => loadColumn(destination.toColumnId))
+      .catch((error) => alert(`Move failed! ${error.message}`));
+  };
+
+  const createTask = (params) => {
+    const attributes = TaskForm.attributesToSubmit(params);
+    return TasksRepository.create(attributes).then(({ data: { task } }) => loadColumn(TaskPresenter.state(task)));
+  };
+
+  const updateTask = (params) => {
+    const attributes = TaskForm.attributesToSubmit(params);
+    return TasksRepository.update(TaskPresenter.id(params), attributes).then(() =>
+      loadColumn(TaskPresenter.state(params)),
+    );
+  };
+
+  const deleteTask = (task) =>
+    TasksRepository.destroy(TaskPresenter.id(task)).then(() => loadColumn(TaskPresenter.state(task)));
+
+  const loadTask = (id) => TasksRepository.show(id).then(({ data: { task } }) => task);
+
   return {
     loadColumn,
     loadColumnMore,
+    changeTaskState,
+    createTask,
+    updateTask,
+    deleteTask,
+    loadTask,
   };
 };
